6d96e849a209aba77b921a52031586fa90c07669
vinnvi <vin.doisneau@gmail.com>
2021-01-26 13:20:17 +0100

    feat(hubpage): navbar added

--- src/Controller/HubController.php
+++ src/Controller/HubController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class HubController extends AbstractController
 {
+
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+       $this->security = $security;
+    }
+
+
     /**
      * @Route("/", name="app_hub")
      */
     public function index(): Response
     {
-        return $this->render('hub/main.html.twig', []);
+        $user = $this->security->getUser();
+
+        return $this->render('hub/main.html.twig', ['user' => $user]);
     }
 }
